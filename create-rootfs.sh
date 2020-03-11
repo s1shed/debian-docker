21bf1a7119b4c9271a127898538cba4542b00fa1
Edward Shornock <ed.shornock@gmail.com>
2020-03-11 21:44:14 +0200

    Also build i386 for Lenny

--- create-rootfs.sh
+++ create-rootfs.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-debootstrap --variant minbase lenny lenny http://archive.debian.org/debian-archive/debian/
-(cd lenny && tar -cvf - . > ../lenny.tar )
+debootstrap --arch i386 --variant minbase lenny lenny http://archive.debian.org/debian-archive/debian/
+(cd lenny && tar -cvf - . > ../lenny.i386.tar )
+rm -rf lenny
+
+debootstrap --arch amd64 --variant minbase lenny lenny http://archive.debian.org/debian-archive/debian/
+(cd lenny && tar -cvf - . > ../lenny.amd64.tar )
 rm -rf lenny
